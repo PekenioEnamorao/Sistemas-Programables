0d428d4415ba4ca959667fc69212c3e0f1f53f72
Misael Zenil <30887598+PekenioEnamorao@users.noreply.github.com>
2017-08-18 19:08:56 +0200

    Add files via upload

---
+++ sensor_de_temperatura.ino
@@ -0,0 +1,36 @@
+#include <Adafruit_Sensor.h>
+
+#include <DHT.h>
+#define DHTTYPE DHT11
+
+const int DHTPin=5;
+
+DHT dht(DHTPin,DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Test");
+  dht.begin();
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+
+  float h= dht.readHumidity();
+  float t= dht.readTemperature();
+  delay(2000);
+
+if (isnan(h) | isnan(t)){
+  Serial.println("Fallo la lectura del sensor");
+  return;
+}
+
+  Serial.print("Humedad:");
+  Serial.print(h);
+  Serial.print("Temperatura:");
+  Serial.print(t);
+  Serial.print("\n");
+  // put your main code here, to run repeatedly:
+
+}
