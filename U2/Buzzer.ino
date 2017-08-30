3fe3f113ef22ddd082354a1136de04862a0ae911
Misael Zenil <30887598+PekenioEnamorao@users.noreply.github.com>
2017-08-29 20:53:53 -0500

    Add files via upload

---
+++ U2/Buzzer.ino
@@ -0,0 +1,21 @@
+void setup() {
+  pinMode(9,OUTPUT);
+  beep(50);
+  beep(50);
+  beep(50);
+  delay(1000);
+}
+
+void beep(unsigned char pausa) {
+  analogWrite(9,20);
+  delay(pausa);       //espera
+  analogWrite(9,0);   //apaga
+  delay(pausa);       //espera
+  
+
+}
+
+void loop(){
+  beep(200);
+}
+
