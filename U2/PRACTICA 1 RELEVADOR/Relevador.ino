096a890ab6da975b07afd94ca4c3b38fe41d1a16
Misael Zenil <30887598+PekenioEnamorao@users.noreply.github.com>
2017-09-01 08:43:47 -0500

    Add files via upload

---
+++ U2/PRACTICA 1 RELEVADOR/Relevador.ino
@@ -0,0 +1,19 @@
+const int pinSensor = 3;    
+const int pinTransistor = 4;
+
+int estado;
+
+void setup() {
+  pinMode(pinSensor, INPUT);
+  pinMode(pinTransistor, OUTPUT);
+  digitalWrite(pinTransistor, LOW);
+}
+
+void loop() {
+  estado = digitalRead(pinSensor);    //LEEMOS EL ESTADO DEL SENSOR PIR
+  if(estado == HIGH){                 //SI HAY MOVIMIENTO 
+    digitalWrite(pinTransistor, HIGH); //ENCENDEMOS EL TRANSITOR
+    } else {                          //SI NO HAY MOVIMIENTO
+    digitalWrite(pinTransistor, LOW);  //APAGAMOS EL TRANSISTOR
+      }
+}
