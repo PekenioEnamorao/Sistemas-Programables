a3a6122f195a0d22abd9c98332c21f1e842db811
RUBICELA <30851218+RUBICELA@users.noreply.github.com>
2017-09-01 14:40:49 -0500

    Add files via upload

--- U2/Buzzer.ino
+++ U2/Buzzer.ino
@@ -1,21 +1,20 @@
+int speakerPin =9;
+
 void setup() {
-  pinMode(9,OUTPUT);
-  beep(50);
-  beep(50);
-  beep(50);
-  delay(1000);
+  // put your setup code here, to run once:
+pinMode(9, OUTPUT);
+beep(50);
+beep(50);
+beep(50);
+delay(1000);
 }
-
-void beep(unsigned char pausa) {
+void beep(unsigned char pausa){
   analogWrite(9,20);
-  delay(pausa);       //espera
-  analogWrite(9,0);   //apaga
-  delay(pausa);       //espera
-  
-
+delay(pausa);
+analogWrite(9,0);
+delay(pausa);
 }
-
-void loop(){
-  beep(200);
+void loop() {
+  // put your main code here, to run repeatedly:
+beep(200);
 }
-
